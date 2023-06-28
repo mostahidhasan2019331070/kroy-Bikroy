7b3f1004db284ce80394fcc1d8e274e9b991dcb7
REDOWAN038 <redowanibrahim2724@gmail.com>
2023-06-28 16:09:36 +0600

    frontend setup

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import { Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+// import Contact from "./pages/Contact"
+// import Policy from "./pages/Policy"
+import PageNotFound from "./pages/PageNotFound"
+// import Register from "./pages/auth/Register"
+// import Login from "./pages/auth/Login"
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        {/* <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/policy' element={<Policy />} /> */}
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
