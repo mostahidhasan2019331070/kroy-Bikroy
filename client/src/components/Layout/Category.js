27c8705b84bfc3172ae4b3df4a3c7b7012274c46
REDOWAN038 <redowanibrahim2724@gmail.com>
2023-07-10 19:34:44 +0600

    initial category line setup

---
+++ client/src/components/Layout/Category.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Category = () => {
+  return (
+    <div className='category'>
+      <ul className='category-section'>
+        <li className='category-items'>Category</li>
+        <li className='category-items'>Category</li>
+        <li className='category-items'>Category</li>
+        <li className='category-items'>Category</li>
+        <li className='category-items'>Category</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Category
